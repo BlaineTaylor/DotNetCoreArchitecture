7ff1b85255d5cb6d77fb232c38cdbcb766d1c99a
Blaine Taylor <Blaine.Taylor@meritagehomes.com>
2021-06-24 15:30:50 -0700

    Finished Module 6

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 using System;
@@ -12,12 +14,14 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
 
-        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository)
+        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository, IEmailService emailService)
         {
             _mapper = mapper;
             _eventRepository = eventRepository;
+            _emailService = emailService;
         }
 
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
@@ -31,6 +35,17 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
             var @event = _mapper.Map<Event>(request);
 
             @event = await _eventRepository.AddAsync(@event);
+
+            var email = new Email() { To = "blainert25@hotmail.com", Body = $"A new event was created: {request}", Subject = "A new event was created" };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                //this shouldn't stop the API from doing else so this can be logged
+            }
 
             return @event.EventId;
         }
